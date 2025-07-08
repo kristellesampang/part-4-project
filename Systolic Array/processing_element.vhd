38891b7b2101dfc864cc28d831ac03802909ab87
pchh520 <pchh520@aucklanduni.ac.nz>
2025-07-09 01:25:10 +1200

    made some changes to make it work. Will need to make some further adjustments to make it not so hard coded and add a control unit and external memory to further advance the system

--- Systolic Array/processing_element.vhd
+++ Systolic Array/processing_element.vhd
@@ -15,57 +15,44 @@ port(
     -- inputs for the MAC operation
     in_data : in std_logic_vector(7 downto 0); -- can be data input or activation 
     in_weight : in std_logic_vector(7 downto 0); -- weight
-    in_accumulator : in std_logic_vector(31 downto 0); -- accumulated value 
+    --in_accumulator : in std_logic_vector(31 downto 0); -- accumulated value 
     
     -- outputs for the MAC operation
     out_data : out std_logic_vector(7 downto 0);
     out_weight : out std_logic_vector(7 downto 0);
+    
+    -- The accumulated MAC result
     result_register : out std_logic_vector(31 downto 0) -- this stays within the PE, the systolic array does not do anything with this 
 );
 end processing_element;
     
 architecture behaviour of processing_element is
-    -- -- initialise signals and vairables
+
+    -- -- initialise signals and vairables for the internal registers
     signal data, weight : std_logic_vector(7 downto 0) := (others => '0');
     signal accumulator : unsigned(31 downto 0) := (others => '0');
-    -- signal multiplication : unsigned(31 downto 0) := (others => '0');
-    signal result_valid : std_logic := '0';
 
-
-    -- Function to check for undefined 'X' bits
-    function contains_X(signal_vec: std_logic_vector) return boolean is
-    begin
-        for i in signal_vec'range loop
-            if signal_vec(i) = 'X' then
-                return true;
-            end if;
-        end loop;
-        return false;
-    end function;
 
 begin
-    -- instantiate all the processing elements
-    
     process(clk, reset)
-        variable multiplication : unsigned(31 downto 0) := (others => '0');
+        variable multiplication : unsigned(31 downto 0);
     begin
         if reset = '1' then
             data <= (others => '0');
             weight <= (others => '0');
             accumulator <= (others => '0');
-            result_valid <= '0';
+
         elsif rising_edge(clk) then
+            -- Receive the incoming data and weight
             data <= in_data;
             weight <= in_weight;
-            
-            if result_valid = '0' then
-                if not contains_X(in_data) and not contains_X(in_weight) and not contains_X(in_accumulator) then
-                    multiplication := resize(unsigned(in_data) * unsigned(in_weight), 32);
-                    accumulator <= multiplication + unsigned(in_accumulator);
-                    if multiplication /= 0 then  -- crude way to detect valid data passed
-                        result_valid <= '1';     -- lock once valid result seen
-                    end if;
-                end if;
+
+            if in_data /= x"00" and in_weight /= x"00" then
+                -- Compute the multplication result (the product)
+                multiplication := resize(unsigned(in_data) * unsigned(in_weight), 32);
+
+                -- Add product to accumulator
+                accumulator <= accumulator + multiplication;
             end if;
         end if;
     end process;
@@ -75,5 +62,4 @@ begin
     out_weight <= weight;
     result_register <= std_logic_vector(accumulator);
     -- result_register <= std_logic_vector(multiplication);
-
 end behaviour;
--- Systolic Array/systolic_array.vhd
+++ Systolic Array/systolic_array.vhd
@@ -25,107 +25,46 @@ port(
 end systolic_array;
     
 architecture behaviour of systolic_array is
-    -- initialise signals and vairables
+
+    -- just a random name for a random custom type to hold a row or column of elements 
+    type some_array_name is array (0 to 2) of std_logic_vector(7 downto 0);
+
+    -- shift registers for feeding data and weights
+    signal data_shift : some_array_name := (others => (others => '0'));
+    signal weight_shift : some_array_name := (others => (others => '0'));
+    
+
     -- Horizontal connections: data flows left to right
-    signal PE00_2_PE01_data        : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE01_2_PE02_data        : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE10_2_PE11_data        : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE11_2_PE12_data        : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE20_2_PE21_data        : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE21_2_PE22_data        : std_logic_vector(7 downto 0) := (others => '0');
+    signal PE00_2_PE01_data, PE01_2_PE02_data : std_logic_vector(7 downto 0) := (others => '0');
+    signal PE10_2_PE11_data, PE11_2_PE12_data : std_logic_vector(7 downto 0) := (others => '0');
+    signal PE20_2_PE21_data, PE21_2_PE22_data : std_logic_vector(7 downto 0) := (others => '0');
 
     -- Vertical connections: data flows top to bottom
-    signal PE00_2_PE10_weight      : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE10_2_PE20_weight      : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE01_2_PE11_weight      : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE11_2_PE21_weight      : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE02_2_PE12_weight      : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE12_2_PE22_weight      : std_logic_vector(7 downto 0) := (others => '0');
+    signal PE00_2_PE10_weight, PE10_2_PE20_weight : std_logic_vector(7 downto 0) := (others => '0');
+    signal PE01_2_PE11_weight, PE11_2_PE21_weight : std_logic_vector(7 downto 0) := (others => '0');
+    signal PE02_2_PE12_weight, PE12_2_PE22_weight : std_logic_vector(7 downto 0) := (others => '0');
     
-    -- Initialise the inputs from the matrix
-    signal PE00_data : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE10_data : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE20_data : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE00_weight : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE01_weight : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE02_weight : std_logic_vector(7 downto 0) := (others => '0');
-
     -- Result register of eahh PE
-    signal PE00_result : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE01_result : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE02_result : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE10_result : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE11_result : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE12_result : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE20_result : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE21_result : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE22_result : std_logic_vector(31 downto 0) := (others => '0');
-
-    -- Accumulator of each PE
-    signal PE00_accumulator : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE01_accumulator : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE02_accumulator : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE10_accumulator : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE11_accumulator : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE12_accumulator : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE20_accumulator : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE21_accumulator : std_logic_vector(31 downto 0) := (others => '0');
-    signal PE22_accumulator : std_logic_vector(31 downto 0) := (others => '0');
-
-    -- Dummy signals to move data and weight outside the systolic array
-    signal PE02_2_dummy : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE12_2_dummy : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE22_2_dummy_data : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE22_2_dummy_weight : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE20_2_dummy : std_logic_vector(7 downto 0) := (others => '0');
-    signal PE21_2_dummy : std_logic_vector(7 downto 0) := (others => '0');
+    signal PE00_result, PE01_result, PE02_result : std_logic_vector(31 downto 0) := (others => '0');
+    signal PE10_result, PE11_result, PE12_result : std_logic_vector(31 downto 0) := (others => '0');
+    signal PE20_result, PE21_result, PE22_result : std_logic_vector(31 downto 0) := (others => '0');
 
     -- use this to manually input to the systolic array
-    signal cycle_count : natural := 0;
-
-    -- Flattened matrix A (data)
-    signal a1, a2, a3 : std_logic_vector(7 downto 0):= (others => '0');
-    signal a4, a5, a6 : std_logic_vector(7 downto 0):= (others => '0');
-    signal a7, a8, a9 : std_logic_vector(7 downto 0):= (others => '0');
-
-    -- Flattened matrix B (weight)
-    signal b1, b2, b3 : std_logic_vector(7 downto 0):= (others => '0');
-    signal b4, b5, b6 : std_logic_vector(7 downto 0):= (others => '0');
-    signal b7, b8, b9 : std_logic_vector(7 downto 0):= (others => '0');
-
+    signal cycle_count : integer := 0;
 
 begin
     
-    -- A: row-major
-    a1 <= matrix_data(0,0);
-    a2 <= matrix_data(0,1);
-    a3 <= matrix_data(0,2);
-    a4 <=matrix_data(1,0);
-    a5 <=matrix_data(1,1);
-    a6 <= matrix_data(1,2);
-    a7 <=matrix_data(2,0);
-    a8 <= matrix_data(2,1);
-    a9 <= matrix_data(2,2);
-
-    -- B: column-major
-    b1 <= matrix_weight(0,0);
-    b2 <= matrix_weight(1,0);
-    b3 <=matrix_weight(2,0);
-    b4 <= matrix_weight(0,1);
-    b5 <= matrix_weight(1,1);
-    b6 <= matrix_weight(2,1);
-    b7 <= matrix_weight(0,2);
-    b8 <= matrix_weight(1,2);
-    b9 <= matrix_weight(2,2);
-
     -- instantiate all PEs
+    --Each PE will take data from left and weight from top and generate it's own sum
+
+    -- Row 0
     PE00: entity work.processing_element
     port map (
         clk             => clk,
         reset           => reset,
-        in_data         => PE00_data,
-        in_weight       => PE00_weight,
-        in_accumulator  => PE00_accumulator,
+        in_data         => data_shift(0),
+        in_weight       => weight_shift(0),
+        --in_accumulator  => PE00_accumulator,
         out_data        => PE00_2_PE01_data,
         out_weight      => PE00_2_PE10_weight,
         result_register => PE00_result
@@ -136,8 +75,8 @@ begin
         clk             => clk,
         reset           => reset,
         in_data         => PE00_2_PE01_data,
-        in_weight       => PE01_weight,
-        in_accumulator  => PE01_accumulator,
+        in_weight       => weight_shift(1),
+        --in_accumulator  => PE01_accumulator,
         out_data        => PE01_2_PE02_data,
         out_weight      => PE01_2_PE11_weight,
         result_register => PE01_result
@@ -149,20 +88,21 @@ begin
         clk             => clk,
         reset           => reset,
         in_data         => PE01_2_PE02_data,
-        in_weight       => PE02_weight,
-        in_accumulator  => PE02_accumulator,
-        out_data        => PE02_2_dummy,
+        in_weight       => weight_shift(2),
+         --in_accumulator  => PE02_accumulator,
+        out_data        => open,
         out_weight      => PE02_2_PE12_weight,
         result_register => PE02_result
     );
 
+    -- Row 1
     PE10: entity work.processing_element
     port map (
         clk             => clk,
         reset           => reset,
-        in_data         => PE10_data,
+        in_data         => data_shift(1),
         in_weight       => PE00_2_PE10_weight,
-        in_accumulator  => PE10_accumulator,
+        --in_accumulator  => PE10_accumulator,
         out_data        => PE10_2_PE11_data,
         out_weight      => PE10_2_PE20_weight,
         result_register => PE10_result
@@ -174,7 +114,7 @@ begin
         reset           => reset,
         in_data         => PE10_2_PE11_data,
         in_weight       => PE01_2_PE11_weight,
-        in_accumulator  => PE11_accumulator,
+        --in_accumulator  => PE11_accumulator,
         out_data        => PE11_2_PE12_data,
         out_weight      => PE11_2_PE21_weight,
         result_register => PE11_result 
@@ -186,21 +126,22 @@ begin
         reset           => reset,
         in_data         => PE11_2_PE12_data,
         in_weight       => PE02_2_PE12_weight,
-        in_accumulator  => PE12_accumulator,
-        out_data        => PE12_2_dummy,
+        --in_accumulator  => PE12_accumulator,
+        out_data        => open,
         out_weight      => PE12_2_PE22_weight,
         result_register => PE12_result
     );
 
+    -- Row 2
     PE20: entity work.processing_element
     port map (
         clk             => clk,
         reset           => reset,
-        in_data         => PE20_data,
+        in_data         => data_shift(2),
         in_weight       => PE10_2_PE20_weight,
-        in_accumulator  => PE20_accumulator,
+        --in_accumulator  => PE20_accumulator,
         out_data        => PE20_2_PE21_data,
-        out_weight      => PE20_2_dummy,
+        out_weight      => open,
         result_register => PE20_result
     );
 
@@ -210,9 +151,9 @@ begin
         reset           => reset,
         in_data         => PE20_2_PE21_data,
         in_weight       => PE11_2_PE21_weight,
-        in_accumulator  => PE21_accumulator,
+        --in_accumulator  => PE21_accumulator,
         out_data        => PE21_2_PE22_data,
-        out_weight      => PE21_2_dummy,
+        out_weight      => open,
         result_register => PE21_result
     );
 
@@ -222,80 +163,75 @@ begin
         reset           => reset,
         in_data         => PE21_2_PE22_data,
         in_weight       => PE12_2_PE22_weight,
-        in_accumulator  => PE22_accumulator,
-        out_data        => PE22_2_dummy_data,
-        out_weight      => PE22_2_dummy_weight,
+        --in_accumulator  => PE22_accumulator,
+        out_data        => open,
+        out_weight      => open,
         result_register => PE22_result
     );
 
-    process(clk, reset) 
-        begin
-            if reset = '1' then 
-                cycle_count <= 0;
-                PE00_data   <= a1;
-                PE10_data   <= a4;
-                PE20_data   <= a7;
-                PE00_weight <= b1;
-                PE01_weight <= b4;
-                PE02_weight <= b7;
+-- Process that feeds in the inputs (will probably turn into a control unit later on as per morteza's suggestion)
+process(clk, reset) 
+begin
+    if reset = '1' then 
+        cycle_count <= 0;
 
-            elsif rising_edge(clk) then    	
-                if cycle_count < 1000 then
-                    cycle_count <= cycle_count + 1;
-                end if;
-                case cycle_count is
-                    -- when cycle count is 0, it is the init clock cycle
-                    when 1 =>
-                        PE00_data   <= (matrix_data(0,0));
-                        PE00_weight <= (matrix_weight(0,0));
+        -- Clear all shift registers on reset
+        data_shift(0)  <= (others => '0');
+        data_shift(1)  <= (others => '0');
+        data_shift(2)  <= (others => '0');
 
-                    when 2 =>
-                        PE00_data   <= (matrix_data(0,1));
-                        PE10_data   <= (matrix_data(1,0));
-                        PE00_weight <= (matrix_weight(1,0));
-                        PE01_weight <= (matrix_weight(0,1));
+        weight_shift(0) <= (others => '0');
+        weight_shift(1) <= (others => '0');
+        weight_shift(2) <= (others => '0');
 
-                    when 3 =>
-                        PE00_data   <= (matrix_data(0,2));
-                        PE10_data   <= (matrix_data(1,1));
-                        PE20_data   <= (matrix_data(2,0));
-                        PE00_weight <= (matrix_weight(2,0));
-                        PE01_weight <= (matrix_weight(1,1));
-                        PE02_weight <= (matrix_weight(0,2));
+    elsif rising_edge(clk) then    	
+        cycle_count <= cycle_count + 1;
 
-                    when 4 =>
-                        PE10_data   <= (matrix_data(1,2));
-                        PE20_data   <= (matrix_data(2,1));
-                        PE01_weight <= (matrix_weight(2,1));
-                        PE02_weight <= (matrix_weight(1,2));
+        -- Feed row 0 of A (data), one element per cycle
+        if cycle_count < 3 then
+            data_shift(0) <= matrix_data(0, cycle_count);
+        else
+            data_shift(0) <= (others => '0');
+        end if;
 
-                    when 5 =>
-                        PE20_data   <= (matrix_data(2,2));
-                        PE02_weight <= (matrix_weight(2,2));
+        -- Feed row 1 of A with 1-cycle delay
+        if cycle_count >= 1 and cycle_count < 4 then
+            data_shift(1) <= matrix_data(1, cycle_count-1);
+        else
+            data_shift(1) <= (others => '0');
+        end if;
 
+        -- Feed row 2 of A with 2-cycle delay
+        if cycle_count >= 2 and cycle_count < 5 then
+            data_shift(2) <= matrix_data(2, cycle_count-2);
+        else
+            data_shift(2) <= (others => '0');
+        end if;
 
-                    when others =>
-                        -- no new data/weight injection
-                        PE00_data   <= (others => '0');
-                        PE10_data   <= (others => '0');
-                        PE20_data   <= (others => '0');
-                        PE00_weight <= (others => '0');
-                        PE01_weight <= (others => '0');
-                        PE02_weight <= (others => '0');
-                end case;
-                PE00_accumulator <= PE00_result;
-                PE01_accumulator <= PE01_result;
-                PE02_accumulator <= PE02_result;
-                PE10_accumulator <= PE10_result;
-                PE11_accumulator <= PE11_result;
-                PE12_accumulator <= PE12_result;
-                PE20_accumulator <= PE20_result;
-                PE21_accumulator <= PE21_result;
-                PE22_accumulator <= PE22_result;
-                
-            end if;
-           
-    end process;
+        -- feed col 0 of B (weight), one element per cycle
+        if cycle_count < 3 then
+            weight_shift(0) <= matrix_weight(cycle_count, 0);
+        else
+            weight_shift(0) <= (others => '0');
+        end if;
+
+        -- Feed col 1 of B with 1-cycle delay
+        if cycle_count >= 1 and cycle_count < 4 then
+            weight_shift(1) <= matrix_weight(cycle_count-1, 1);
+        else
+            weight_shift(1) <= (others => '0');
+        end if;
+
+        -- Feed col 2 of B with 2-cycle delay
+        if cycle_count >= 2 and cycle_count < 5 then
+            weight_shift(2) <= matrix_weight(cycle_count-2, 2);
+        else
+            weight_shift(2) <= (others => '0');
+        end if;
+    end if;
+end process;
+
+        -- assign them result registers of each PE to the output matrix
 
     -- output assignment, which is the the result registers
     output(0,0) <= (PE00_result);
