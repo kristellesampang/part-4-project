585606dd181a13871ef72eb74ca4cb05524c6bf3
pchh520 <pchh520@aucklanduni.ac.nz>
2026-01-05 12:41:05 +1300

    stuff i did before break

--- SummerResearch/quartus/PR_Test_Top/PR_Test_Top_inst.v
+++ SummerResearch/quartus/PR_Test_Top/PR_Test_Top_inst.v
@@ -1,24 +1,25 @@
 	PR_Test_Top u0 (
-		.ddr4_emif_global_reset_n_reset_n (_connected_to_ddr4_emif_global_reset_n_reset_n_), //   input,   width = 1, ddr4_emif_global_reset_n.reset_n
-		.ddr4_emif_pll_ref_clk_clk        (_connected_to_ddr4_emif_pll_ref_clk_clk_),        //   input,   width = 1,    ddr4_emif_pll_ref_clk.clk
-		.ddr4_emif_oct_oct_rzqin          (_connected_to_ddr4_emif_oct_oct_rzqin_),          //   input,   width = 1,            ddr4_emif_oct.oct_rzqin
-		.ddr4_emif_mem_mem_ck             (_connected_to_ddr4_emif_mem_mem_ck_),             //  output,   width = 1,            ddr4_emif_mem.mem_ck
-		.ddr4_emif_mem_mem_ck_n           (_connected_to_ddr4_emif_mem_mem_ck_n_),           //  output,   width = 1,                         .mem_ck_n
-		.ddr4_emif_mem_mem_a              (_connected_to_ddr4_emif_mem_mem_a_),              //  output,  width = 17,                         .mem_a
-		.ddr4_emif_mem_mem_act_n          (_connected_to_ddr4_emif_mem_mem_act_n_),          //  output,   width = 1,                         .mem_act_n
-		.ddr4_emif_mem_mem_ba             (_connected_to_ddr4_emif_mem_mem_ba_),             //  output,   width = 2,                         .mem_ba
-		.ddr4_emif_mem_mem_bg             (_connected_to_ddr4_emif_mem_mem_bg_),             //  output,   width = 1,                         .mem_bg
-		.ddr4_emif_mem_mem_cke            (_connected_to_ddr4_emif_mem_mem_cke_),            //  output,   width = 1,                         .mem_cke
-		.ddr4_emif_mem_mem_cs_n           (_connected_to_ddr4_emif_mem_mem_cs_n_),           //  output,   width = 1,                         .mem_cs_n
-		.ddr4_emif_mem_mem_odt            (_connected_to_ddr4_emif_mem_mem_odt_),            //  output,   width = 1,                         .mem_odt
-		.ddr4_emif_mem_mem_reset_n        (_connected_to_ddr4_emif_mem_mem_reset_n_),        //  output,   width = 1,                         .mem_reset_n
-		.ddr4_emif_mem_mem_par            (_connected_to_ddr4_emif_mem_mem_par_),            //  output,   width = 1,                         .mem_par
-		.ddr4_emif_mem_mem_alert_n        (_connected_to_ddr4_emif_mem_mem_alert_n_),        //   input,   width = 1,                         .mem_alert_n
-		.ddr4_emif_mem_mem_dqs            (_connected_to_ddr4_emif_mem_mem_dqs_),            //   inout,   width = 8,                         .mem_dqs
-		.ddr4_emif_mem_mem_dqs_n          (_connected_to_ddr4_emif_mem_mem_dqs_n_),          //   inout,   width = 8,                         .mem_dqs_n
-		.ddr4_emif_mem_mem_dq             (_connected_to_ddr4_emif_mem_mem_dq_),             //   inout,  width = 64,                         .mem_dq
-		.ddr4_emif_mem_mem_dbi_n          (_connected_to_ddr4_emif_mem_mem_dbi_n_),          //   inout,   width = 8,                         .mem_dbi_n
-		.clk_clk                          (_connected_to_clk_clk_),                          //   input,   width = 1,                      clk.clk
-		.reset_reset                      (_connected_to_reset_reset_)                       //   input,   width = 1,                    reset.reset
+		.sdram_pll_locked_pll_locked    (_connected_to_sdram_pll_locked_pll_locked_),    //  output,   width = 1, sdram_pll_locked.pll_locked
+		.ddr4_emif_oct_oct_rzqin        (_connected_to_ddr4_emif_oct_oct_rzqin_),        //   input,   width = 1,    ddr4_emif_oct.oct_rzqin
+		.ddr4_emif_mem_mem_ck           (_connected_to_ddr4_emif_mem_mem_ck_),           //  output,   width = 1,    ddr4_emif_mem.mem_ck
+		.ddr4_emif_mem_mem_ck_n         (_connected_to_ddr4_emif_mem_mem_ck_n_),         //  output,   width = 1,                 .mem_ck_n
+		.ddr4_emif_mem_mem_a            (_connected_to_ddr4_emif_mem_mem_a_),            //  output,  width = 17,                 .mem_a
+		.ddr4_emif_mem_mem_act_n        (_connected_to_ddr4_emif_mem_mem_act_n_),        //  output,   width = 1,                 .mem_act_n
+		.ddr4_emif_mem_mem_ba           (_connected_to_ddr4_emif_mem_mem_ba_),           //  output,   width = 2,                 .mem_ba
+		.ddr4_emif_mem_mem_bg           (_connected_to_ddr4_emif_mem_mem_bg_),           //  output,   width = 1,                 .mem_bg
+		.ddr4_emif_mem_mem_cke          (_connected_to_ddr4_emif_mem_mem_cke_),          //  output,   width = 1,                 .mem_cke
+		.ddr4_emif_mem_mem_cs_n         (_connected_to_ddr4_emif_mem_mem_cs_n_),         //  output,   width = 1,                 .mem_cs_n
+		.ddr4_emif_mem_mem_odt          (_connected_to_ddr4_emif_mem_mem_odt_),          //  output,   width = 1,                 .mem_odt
+		.ddr4_emif_mem_mem_reset_n      (_connected_to_ddr4_emif_mem_mem_reset_n_),      //  output,   width = 1,                 .mem_reset_n
+		.ddr4_emif_mem_mem_par          (_connected_to_ddr4_emif_mem_mem_par_),          //  output,   width = 1,                 .mem_par
+		.ddr4_emif_mem_mem_alert_n      (_connected_to_ddr4_emif_mem_mem_alert_n_),      //   input,   width = 1,                 .mem_alert_n
+		.ddr4_emif_mem_mem_dqs          (_connected_to_ddr4_emif_mem_mem_dqs_),          //   inout,   width = 8,                 .mem_dqs
+		.ddr4_emif_mem_mem_dqs_n        (_connected_to_ddr4_emif_mem_mem_dqs_n_),        //   inout,   width = 8,                 .mem_dqs_n
+		.ddr4_emif_mem_mem_dq           (_connected_to_ddr4_emif_mem_mem_dq_),           //   inout,  width = 64,                 .mem_dq
+		.ddr4_emif_mem_mem_dbi_n        (_connected_to_ddr4_emif_mem_mem_dbi_n_),        //   inout,   width = 8,                 .mem_dbi_n
+		.sdram_status_local_cal_success (_connected_to_sdram_status_local_cal_success_), //  output,   width = 1,     sdram_status.local_cal_success
+		.sdram_status_local_cal_fail    (_connected_to_sdram_status_local_cal_fail_),    //  output,   width = 1,                 .local_cal_fail
+		.clk_clk                        (_connected_to_clk_clk_),                        //   input,   width = 1,              clk.clk
+		.reset_reset                    (_connected_to_reset_reset_)                     //   input,   width = 1,            reset.reset
 	);
 
--- SummerResearch/quartus/PR_Test_Top/PR_Test_Top_inst.vhd
+++ SummerResearch/quartus/PR_Test_Top/PR_Test_Top_inst.vhd
@@ -1,51 +1,53 @@
 	component PR_Test_Top is
 		port (
-			ddr4_emif_global_reset_n_reset_n : in    std_logic                     := 'X';             -- reset_n
-			ddr4_emif_pll_ref_clk_clk        : in    std_logic                     := 'X';             -- clk
-			ddr4_emif_oct_oct_rzqin          : in    std_logic                     := 'X';             -- oct_rzqin
-			ddr4_emif_mem_mem_ck             : out   std_logic_vector(0 downto 0);                     -- mem_ck
-			ddr4_emif_mem_mem_ck_n           : out   std_logic_vector(0 downto 0);                     -- mem_ck_n
-			ddr4_emif_mem_mem_a              : out   std_logic_vector(16 downto 0);                    -- mem_a
-			ddr4_emif_mem_mem_act_n          : out   std_logic_vector(0 downto 0);                     -- mem_act_n
-			ddr4_emif_mem_mem_ba             : out   std_logic_vector(1 downto 0);                     -- mem_ba
-			ddr4_emif_mem_mem_bg             : out   std_logic_vector(0 downto 0);                     -- mem_bg
-			ddr4_emif_mem_mem_cke            : out   std_logic_vector(0 downto 0);                     -- mem_cke
-			ddr4_emif_mem_mem_cs_n           : out   std_logic_vector(0 downto 0);                     -- mem_cs_n
-			ddr4_emif_mem_mem_odt            : out   std_logic_vector(0 downto 0);                     -- mem_odt
-			ddr4_emif_mem_mem_reset_n        : out   std_logic_vector(0 downto 0);                     -- mem_reset_n
-			ddr4_emif_mem_mem_par            : out   std_logic_vector(0 downto 0);                     -- mem_par
-			ddr4_emif_mem_mem_alert_n        : in    std_logic_vector(0 downto 0)  := (others => 'X'); -- mem_alert_n
-			ddr4_emif_mem_mem_dqs            : inout std_logic_vector(7 downto 0)  := (others => 'X'); -- mem_dqs
-			ddr4_emif_mem_mem_dqs_n          : inout std_logic_vector(7 downto 0)  := (others => 'X'); -- mem_dqs_n
-			ddr4_emif_mem_mem_dq             : inout std_logic_vector(63 downto 0) := (others => 'X'); -- mem_dq
-			ddr4_emif_mem_mem_dbi_n          : inout std_logic_vector(7 downto 0)  := (others => 'X'); -- mem_dbi_n
-			clk_clk                          : in    std_logic                     := 'X';             -- clk
-			reset_reset                      : in    std_logic                     := 'X'              -- reset
+			sdram_pll_locked_pll_locked    : out   std_logic;                                        -- pll_locked
+			ddr4_emif_oct_oct_rzqin        : in    std_logic                     := 'X';             -- oct_rzqin
+			ddr4_emif_mem_mem_ck           : out   std_logic_vector(0 downto 0);                     -- mem_ck
+			ddr4_emif_mem_mem_ck_n         : out   std_logic_vector(0 downto 0);                     -- mem_ck_n
+			ddr4_emif_mem_mem_a            : out   std_logic_vector(16 downto 0);                    -- mem_a
+			ddr4_emif_mem_mem_act_n        : out   std_logic_vector(0 downto 0);                     -- mem_act_n
+			ddr4_emif_mem_mem_ba           : out   std_logic_vector(1 downto 0);                     -- mem_ba
+			ddr4_emif_mem_mem_bg           : out   std_logic_vector(0 downto 0);                     -- mem_bg
+			ddr4_emif_mem_mem_cke          : out   std_logic_vector(0 downto 0);                     -- mem_cke
+			ddr4_emif_mem_mem_cs_n         : out   std_logic_vector(0 downto 0);                     -- mem_cs_n
+			ddr4_emif_mem_mem_odt          : out   std_logic_vector(0 downto 0);                     -- mem_odt
+			ddr4_emif_mem_mem_reset_n      : out   std_logic_vector(0 downto 0);                     -- mem_reset_n
+			ddr4_emif_mem_mem_par          : out   std_logic_vector(0 downto 0);                     -- mem_par
+			ddr4_emif_mem_mem_alert_n      : in    std_logic_vector(0 downto 0)  := (others => 'X'); -- mem_alert_n
+			ddr4_emif_mem_mem_dqs          : inout std_logic_vector(7 downto 0)  := (others => 'X'); -- mem_dqs
+			ddr4_emif_mem_mem_dqs_n        : inout std_logic_vector(7 downto 0)  := (others => 'X'); -- mem_dqs_n
+			ddr4_emif_mem_mem_dq           : inout std_logic_vector(63 downto 0) := (others => 'X'); -- mem_dq
+			ddr4_emif_mem_mem_dbi_n        : inout std_logic_vector(7 downto 0)  := (others => 'X'); -- mem_dbi_n
+			sdram_status_local_cal_success : out   std_logic;                                        -- local_cal_success
+			sdram_status_local_cal_fail    : out   std_logic;                                        -- local_cal_fail
+			clk_clk                        : in    std_logic                     := 'X';             -- clk
+			reset_reset                    : in    std_logic                     := 'X'              -- reset
 		);
 	end component PR_Test_Top;
 
 	u0 : component PR_Test_Top
 		port map (
-			ddr4_emif_global_reset_n_reset_n => CONNECTED_TO_ddr4_emif_global_reset_n_reset_n, -- ddr4_emif_global_reset_n.reset_n
-			ddr4_emif_pll_ref_clk_clk        => CONNECTED_TO_ddr4_emif_pll_ref_clk_clk,        --    ddr4_emif_pll_ref_clk.clk
-			ddr4_emif_oct_oct_rzqin          => CONNECTED_TO_ddr4_emif_oct_oct_rzqin,          --            ddr4_emif_oct.oct_rzqin
-			ddr4_emif_mem_mem_ck             => CONNECTED_TO_ddr4_emif_mem_mem_ck,             --            ddr4_emif_mem.mem_ck
-			ddr4_emif_mem_mem_ck_n           => CONNECTED_TO_ddr4_emif_mem_mem_ck_n,           --                         .mem_ck_n
-			ddr4_emif_mem_mem_a              => CONNECTED_TO_ddr4_emif_mem_mem_a,              --                         .mem_a
-			ddr4_emif_mem_mem_act_n          => CONNECTED_TO_ddr4_emif_mem_mem_act_n,          --                         .mem_act_n
-			ddr4_emif_mem_mem_ba             => CONNECTED_TO_ddr4_emif_mem_mem_ba,             --                         .mem_ba
-			ddr4_emif_mem_mem_bg             => CONNECTED_TO_ddr4_emif_mem_mem_bg,             --                         .mem_bg
-			ddr4_emif_mem_mem_cke            => CONNECTED_TO_ddr4_emif_mem_mem_cke,            --                         .mem_cke
-			ddr4_emif_mem_mem_cs_n           => CONNECTED_TO_ddr4_emif_mem_mem_cs_n,           --                         .mem_cs_n
-			ddr4_emif_mem_mem_odt            => CONNECTED_TO_ddr4_emif_mem_mem_odt,            --                         .mem_odt
-			ddr4_emif_mem_mem_reset_n        => CONNECTED_TO_ddr4_emif_mem_mem_reset_n,        --                         .mem_reset_n
-			ddr4_emif_mem_mem_par            => CONNECTED_TO_ddr4_emif_mem_mem_par,            --                         .mem_par
-			ddr4_emif_mem_mem_alert_n        => CONNECTED_TO_ddr4_emif_mem_mem_alert_n,        --                         .mem_alert_n
-			ddr4_emif_mem_mem_dqs            => CONNECTED_TO_ddr4_emif_mem_mem_dqs,            --                         .mem_dqs
-			ddr4_emif_mem_mem_dqs_n          => CONNECTED_TO_ddr4_emif_mem_mem_dqs_n,          --                         .mem_dqs_n
-			ddr4_emif_mem_mem_dq             => CONNECTED_TO_ddr4_emif_mem_mem_dq,             --                         .mem_dq
-			ddr4_emif_mem_mem_dbi_n          => CONNECTED_TO_ddr4_emif_mem_mem_dbi_n,          --                         .mem_dbi_n
-			clk_clk                          => CONNECTED_TO_clk_clk,                          --                      clk.clk
-			reset_reset                      => CONNECTED_TO_reset_reset                       --                    reset.reset
+			sdram_pll_locked_pll_locked    => CONNECTED_TO_sdram_pll_locked_pll_locked,    -- sdram_pll_locked.pll_locked
+			ddr4_emif_oct_oct_rzqin        => CONNECTED_TO_ddr4_emif_oct_oct_rzqin,        --    ddr4_emif_oct.oct_rzqin
+			ddr4_emif_mem_mem_ck           => CONNECTED_TO_ddr4_emif_mem_mem_ck,           --    ddr4_emif_mem.mem_ck
+			ddr4_emif_mem_mem_ck_n         => CONNECTED_TO_ddr4_emif_mem_mem_ck_n,         --                 .mem_ck_n
+			ddr4_emif_mem_mem_a            => CONNECTED_TO_ddr4_emif_mem_mem_a,            --                 .mem_a
+			ddr4_emif_mem_mem_act_n        => CONNECTED_TO_ddr4_emif_mem_mem_act_n,        --                 .mem_act_n
+			ddr4_emif_mem_mem_ba           => CONNECTED_TO_ddr4_emif_mem_mem_ba,           --                 .mem_ba
+			ddr4_emif_mem_mem_bg           => CONNECTED_TO_ddr4_emif_mem_mem_bg,           --                 .mem_bg
+			ddr4_emif_mem_mem_cke          => CONNECTED_TO_ddr4_emif_mem_mem_cke,          --                 .mem_cke
+			ddr4_emif_mem_mem_cs_n         => CONNECTED_TO_ddr4_emif_mem_mem_cs_n,         --                 .mem_cs_n
+			ddr4_emif_mem_mem_odt          => CONNECTED_TO_ddr4_emif_mem_mem_odt,          --                 .mem_odt
+			ddr4_emif_mem_mem_reset_n      => CONNECTED_TO_ddr4_emif_mem_mem_reset_n,      --                 .mem_reset_n
+			ddr4_emif_mem_mem_par          => CONNECTED_TO_ddr4_emif_mem_mem_par,          --                 .mem_par
+			ddr4_emif_mem_mem_alert_n      => CONNECTED_TO_ddr4_emif_mem_mem_alert_n,      --                 .mem_alert_n
+			ddr4_emif_mem_mem_dqs          => CONNECTED_TO_ddr4_emif_mem_mem_dqs,          --                 .mem_dqs
+			ddr4_emif_mem_mem_dqs_n        => CONNECTED_TO_ddr4_emif_mem_mem_dqs_n,        --                 .mem_dqs_n
+			ddr4_emif_mem_mem_dq           => CONNECTED_TO_ddr4_emif_mem_mem_dq,           --                 .mem_dq
+			ddr4_emif_mem_mem_dbi_n        => CONNECTED_TO_ddr4_emif_mem_mem_dbi_n,        --                 .mem_dbi_n
+			sdram_status_local_cal_success => CONNECTED_TO_sdram_status_local_cal_success, --     sdram_status.local_cal_success
+			sdram_status_local_cal_fail    => CONNECTED_TO_sdram_status_local_cal_fail,    --                 .local_cal_fail
+			clk_clk                        => CONNECTED_TO_clk_clk,                        --              clk.clk
+			reset_reset                    => CONNECTED_TO_reset_reset                     --            reset.reset
 		);
 
